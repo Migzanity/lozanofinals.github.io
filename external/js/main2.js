87447bd0bd9a342c905d36f76d01d84ec55302a1
Migzanity <migzlozano12@gmail.com>
2022-07-14 11:36:14 +0800

    Added new files

---
+++ external/js/main2.js
@@ -0,0 +1,48 @@
+!(function($) {
+  "use strict";
+  
+  // Initialize Venobox
+  $(window).on('load', function() {
+    $('.venobox').venobox({
+      bgcolor: '',
+      overlayColor: 'rgba(6, 12, 34, 0.85)',
+      closeBackground: '',
+      closeColor: '#fff',
+      share: false
+    });
+  });
+
+  // Gallery carousel (uses the Owl Carousel library)
+  $(".gallery-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 3
+      },
+      992: {
+        items: 4
+      },
+      1200: {
+        items: 5
+      }
+    }
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+		duration: 1000,
+		once: true
+    });
+  }
+  $(window).on('load', function() {
+    aos_init();
+  });
+  
+})(jQuery);
